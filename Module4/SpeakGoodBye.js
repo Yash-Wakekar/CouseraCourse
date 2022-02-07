3c5f1144c2631b2558650d2cd1997c0353691d9b
Yash-Wakekar <91482798+Yash-Wakekar@users.noreply.github.com>
2022-02-07 10:46:06 +0530

    module4

---
+++ Module4/SpeakGoodBye.js
@@ -0,0 +1,10 @@
+
+(function(window) {
+	var speakWord = "Good Bye";
+	var byeSpeaker = function (name) {
+  		console.log(speakWord + " " + name);
+	}
+	
+	window.byeSpeaker = byeSpeaker;
+
+})(window);
